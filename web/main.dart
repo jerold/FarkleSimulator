5bdadb3ef4a0e420217ee16a121e7439bcda142e
Jerold Albertson <jerold.albertson@gmail.com>
2019-10-17 21:46:18 -0600

    set up pwa

--- web/main.dart
+++ web/main.dart
@@ -1,10 +1,12 @@
 import 'dart:html';
 
+import 'package:pwa/client.dart' as pwa;
 import 'package:wui_builder/wui_builder.dart';
 
 import 'package:farkle_simulator/farkle_simulator.dart';
 
 void main() {
+  new pwa.Client();
   final ui = new UI();
   render(ui, querySelector('#farkle'));
 }
